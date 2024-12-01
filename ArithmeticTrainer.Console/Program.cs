1b14027f705bbde12ba8380f21b583da0ac7638b
yamesant <165981998+yamesant@users.noreply.github.com>
2024-12-01 21:23:08 +1100

    Implement addition

---
+++ ArithmeticTrainer.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿Random random = new();
+
+while (true)
+{
+    int result = random.Next(4, 100);
+    int x = random.Next(2, result - 1);
+    int y = result - x;
+    Console.WriteLine($"{x} + {y} = ?");
+    string input = Console.ReadLine() ?? "";
+    if (input != result.ToString())
+    {
+        Console.WriteLine($"Wrong, the answer is {result}");
+    }
+    else
+    {
+        Console.WriteLine("Correct");
+    }
+}
