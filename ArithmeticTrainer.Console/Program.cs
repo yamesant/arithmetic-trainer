fd501df2f855194cd2debc954b3d04251fee1634
yamesant <165981998+yamesant@users.noreply.github.com>
2024-12-01 22:13:28 +1100

    Add multiplication

--- ArithmeticTrainer.Console/Program.cs
+++ ArithmeticTrainer.Console/Program.cs
@@ -3,7 +3,7 @@
 while (true)
 {
     Console.WriteLine("Welcome to Arithmetic Trainer");
-    Console.WriteLine("Pick operation to practice: +, -, /");
+    Console.WriteLine("Pick operation to practice: +, -, *, /");
     string input = Console.ReadLine() ?? "";
     switch (input)
     {
@@ -12,6 +12,9 @@ while (true)
             break;
         case "-":
             DoSubtraction();
+            break;
+        case "*":
+            DoMultiplication();
             break;
         case "/":
             DoDivision();
@@ -56,6 +59,52 @@ void DoSubtraction()
         int x = random.Next(result + 2, 100);
         int y = x - result;
         Console.WriteLine($"{x} - {y} = ?");
+        string input = Console.ReadLine() ?? "";
+        if (input == "q")
+        {
+            break;
+        }
+        if (input != result.ToString())
+        {
+            Console.WriteLine($"Wrong, the answer is {result}");
+        }
+        else
+        {
+            Console.WriteLine("Correct");
+        }
+    }
+}
+
+(int, int, int) GenerateMultiplicationTriple()
+{
+    while (true)
+    {
+        int product = random.Next(4, 100);
+        List<int> divisors = [];
+        for (int d = 2; d <= product / 2; d++)
+        {
+            if (product % d == 0)
+            {
+                divisors.Add(d);
+            }
+        }
+        if (divisors.Count == 0)
+        {
+            continue;
+        }
+        int x = divisors[random.Next(0, divisors.Count)];
+        int y = product / x;
+        return (x, y, product);
+    }
+}
+
+void DoMultiplication()
+{
+    Console.WriteLine("Starting multiplication practice. Press q to stop.");
+    while (true)
+    {
+        (int x, int y, int result) = GenerateMultiplicationTriple();
+        Console.WriteLine($"{x} * {y} = ?");
         string input = Console.ReadLine() ?? "";
         if (input == "q")
         {
