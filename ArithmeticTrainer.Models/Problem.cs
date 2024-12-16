f7a2c6072341c8e8b21715549588bd4f4c17e14e
yamesant <165981998+yamesant@users.noreply.github.com>
2024-12-16 16:25:11 +1100

    Remove generic problem

--- ArithmeticTrainer.Models/Problem.cs
+++ ArithmeticTrainer.Models/Problem.cs
@@ -5,9 +5,3 @@ public abstract class Problem
     public abstract string Question { get; }
     public abstract string Answer { get; }
 }
-
-public sealed class GenericProblem(string question, string answer) : Problem
-{
-    public override string Question => question;
-    public override string Answer => answer;
-}
